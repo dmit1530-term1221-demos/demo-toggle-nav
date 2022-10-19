7edc1bd150f1ab377ca86a8d3c3c03f36de0a77c
Lauren Olsen <laureno@nait.ca>
2022-10-18 21:30:47 -0600

    add js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
 // Step 1: Defining Constants
+const clickButton = document.querySelector('.toggle-btn');
 
 // Step 2: Adding A Click Event
+clickButton.addEventListener('click', () => {
+    //when the icon is clicked we are going to grab the nav element (or whatever is showing or hiding) and add or remove that special class we created in the css.
+    document.querySelector('nav').classList.toggle('show-nav');
+});
